ab6366a33e2ec6fafdb32ac3a32aa001abeff444
yusuke <yusuke@macbook.lan>
2021-07-30 10:14:44 +0900

    試作

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,7 @@
+import Item_NewsAndEventsMore from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/NewsAndEventsMore" }
+
+export const NewsAndEventsMore = () => Item_NewsAndEventsMore;
+
